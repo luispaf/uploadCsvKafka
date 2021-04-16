defc4f4b81b417c7b0e252f29cd4a5f2f02ecdc2
Luis Paulo <andradefreitas590@gmail.com>
2021-04-15 23:12:12 -0300

    refatoração

--- src/main/java/br/com/uol/pagseguro/customerPortfolio/domain/CustomerPortfolio.java
+++ src/main/java/br/com/uol/pagseguro/customerPortfolio/domain/CustomerPortfolio.java
@@ -13,11 +13,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "customer_portfolio")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
